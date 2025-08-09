b6d87fc7ff10c0f061cb1ab38a44c1aa0d7c0dce
Mohamad <m.hawchar@prfd.aero>
2025-08-09 09:20:20 +0300

    Add baseEntity and IsoftDeletable Update to .net 9

---
+++ AdminPanel.Data/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using AdminPanel.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Data.Entities
+{
+    public class BaseEntity: ISoftDeletable
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
