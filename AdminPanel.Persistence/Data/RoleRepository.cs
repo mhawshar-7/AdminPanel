2c94146571322b2036e803f1e80d4697a75cf18a
Mohamad <m.hawchar@prfd.aero>
2025-08-24 20:11:58 +0300

    Add role service

---
+++ AdminPanel.Persistence/Data/RoleRepository.cs
@@ -0,0 +1,28 @@
+﻿using AdminPanel.Application.Interfaces;
+using AdminPanel.Data.Entities.Identity;
+using AdminPanel.Data.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPanel.Persistence.Data
+{
+    public class RoleRepository: IRoleRepository
+    {
+        private readonly UserManager<User> _userManager;
+
+        public RoleRepository(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> AddToRoleAsync(User user, string roleName)
+        {
+            return await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task<IdentityResult> RemoveFromRoleAsync(User user, string roleName)
+        {
+            return await _userManager.RemoveFromRoleAsync(user, roleName);
+        }
+    }
+}
