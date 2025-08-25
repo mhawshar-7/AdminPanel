8992c784a3faf903199927e1041974c121ecfb4a
Mohamad Hawchar <mhawchar@borninteractive.com>
2025-08-25 17:32:45 +0300

    wip

---
+++ AdminPanel.Web/Models/UserMenuViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminPanel.Web.Models
+{
+    public class UserMenuViewModel
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+    }
+}
